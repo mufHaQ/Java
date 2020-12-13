94ca1330739635529589dce643cae0900d1d5b51
mufHaQ <masbrowmess33@gmail.com>
2020-12-13 15:47:20 +0700

    update: Recursive.java

--- src/myjava1/Recursive.java
+++ src/myjava1/Recursive.java
@@ -18,12 +18,24 @@ public class Recursive {
         System.out.println();
 
         int jmlh = jumlah(nilai);
-        System.out.println("\nJumlah: " + jmlh);
+        System.out.println("\nJumlah-Penjumlahan: " + jmlh);
+        
+        int faktor = faktorial(nilai);
+        System.out.println("Jumlah-Faktorial: " + faktor);
         
         inpt.close();
     }
 
     // Fungsi Rekursif sederhana
+    
+    private static int faktorial(int nilai) {
+    	if (nilai == 1) {
+    		return nilai;
+    	}
+    	
+    	return nilai * faktorial(nilai - 1);
+    }
+    
 
     private static int jumlah(int nilai){
         System.out.println("nilai-2: " + nilai);
